cf94e804a5aafc9c1080d6a809c97aa58f8b517f
Pikri-Habibi <pikrihabibi2006@gmail.com>
2026-09-16 09:05:14 +0700

    Selesaikan CRUD jenis dan harga sampah

--- app/Http/Controllers/JenisSampahController.php
+++ app/Http/Controllers/JenisSampahController.php
@@ -13,4 +13,54 @@ class JenisSampahController extends Controller
 
         return response()->json($data);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_sampah' => 'required|string|max:255',
+        ]);
+
+        $data = JenisSampah::create([
+            'nama_sampah' => $request->nama_sampah,
+        ]);
+
+        return response()->json([
+            'message' => 'Jenis sampah berhasil ditambahkan',
+            'data' => $data
+        ], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_sampah' => 'required|string|max:255',
+        ]);
+
+        $jenisSampah = JenisSampah::findOrFail($id);
+
+        $jenisSampah->update([
+            'nama_sampah' => $request->nama_sampah,
+        ]);
+
+        return response()->json([
+            'message' => 'Jenis sampah berhasil diperbarui',
+            'data' => $jenisSampah
+        ]);
+    }
+    public function destroy($id)
+    {
+        $jenisSampah = JenisSampah::findOrFail($id);
+
+        if ($jenisSampah->harga()->exists()) {
+            return response()->json([
+                'message' => 'Jenis sampah tidak bisa dihapus karena masih memiliki data harga'
+            ], 422);
+        }
+
+        $jenisSampah->delete();
+
+        return response()->json([
+            'message' => 'Jenis sampah berhasil dihapus'
+        ]);
+    }
 }
